7771c7dae9e9e0e587f4d775c40814ce227d881c
doniyoryuldashev95 <doniyoryuldashev95@gmail.com>
2022-09-05 12:25:19 +0500

    doneDone

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year%400){
+            case 0:
+            case 2:
+            case 4:
+            case 6:
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
